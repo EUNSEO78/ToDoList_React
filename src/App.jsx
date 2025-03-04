87a8321503147dd3716e4ca033320b725233711e
EUNSEO78 <dmstjalstj4@gmail.com>
2025-03-04 23:55:21 +0900

    feat: input 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import TodoListTemplate from "./components/TodoListTemplate";
+import TodoListTemplate from "./components/TodoList/TodoListTemplate";
+import "./common/reset.css";
 
 function App() {
   return (
     <div
-      style={{ width: "100vw", height: "100vh", backgroundColor: "#1D1D1F" }}
+      style={{ width: "100vw", height: "100vh", backgroundColor: "#282C37" }}
     >
       <TodoListTemplate />
     </div>
--- src/components/TodoListTemplate.jsx
+++
@@ -1,7 +0,0 @@
-// TodoInsert/TodoInput , TodoListItem/TodoListBox -> TodoList
-
-const TodoListTemplate = () => {
-  return <></>;
-};
-
-export default TodoListTemplate;
