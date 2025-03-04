b35a027535019959c6b94035791df53326da47b2
EUNSEO78 <dmstjalstj4@gmail.com>
2025-03-04 21:58:50 +0900

    Initial commit

---
+++ TodoList/src/App.jsx
@@ -0,0 +1,13 @@
+import TodoListTemplate from "./components/TodoListTemplate";
+
+function App() {
+  return (
+    <div
+      style={{ width: "100vw", height: "100vh", backgroundColor: "#1D1D1F" }}
+    >
+      <TodoListTemplate />
+    </div>
+  );
+}
+
+export default App;
---
+++ TodoList/src/components/TodoListTemplate.jsx
@@ -0,0 +1,7 @@
+// TodoInsert/TodoInput , TodoListItem/TodoListBox -> TodoList
+
+const TodoListTemplate = () => {
+  return <></>;
+};
+
+export default TodoListTemplate;
