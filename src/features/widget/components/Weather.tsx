a8162cc3ba6f27350da81249390ad1fa481fa838
EUNSEO78 <dmstjalstj4@gmail.com>
2025-03-07 00:54:09 +0900

    weather 위젯 구조

--- src/features/widget/components/Weather.tsx
+++ src/features/widget/components/Weather.tsx
@@ -1,9 +1,32 @@
+import { useEffect } from "react";
+
 const Weather = () => {
-  // navigator.geolocation.getCurrentPosition((v) => {
-  //   console.log(v.coords.latitude);
-  //   console.log(v.coords.longitude);
-  // });
-  return;
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((v) => {
+      const lat = v.coords.latitude;
+      const lon = v.coords.longitude;
+      const key = "49d326729db59a0224f9acbc6be4ce6c";
+      weatherData(lat, lon, key);
+    });
+
+    const weatherData = (lat, lon, key) => {
+      console.log(lat, lon);
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data || data.name || data.wind);
+        });
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Weather</h1>
+      <span></span>
+    </div>
+  );
 };
 
 export default Weather;
